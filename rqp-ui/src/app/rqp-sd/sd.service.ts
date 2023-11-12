e5c0ff9c29b176e54491e59c4782e6a02f704c8e
bharat947 <itsbharat.bk@gmail.com>
2023-11-12 11:19:21 +0530

    quotation home page

--- rqp-ui/src/app/rqp-sd/sd.service.ts
+++ rqp-ui/src/app/rqp-sd/sd.service.ts
@@ -13,20 +13,48 @@ export class SdService {
   // private API_URL='http://103.10.234.106:8081/';
   constructor(private http:HttpClient,private cookieService:CookieService) { }
 
-  getStockList(size:any,pageIndex:any){
-    const queryParams = `?pageIndex=${pageIndex}&size=${size}`;
+  getStockList(orgUnitCode:any,pageIndex:any,size:any){
+    let body={
+      'buCode':orgUnitCode
+    }
+   // const queryParams = `?pageIndex=${pageIndex}&size=${size}`;
+    const queryParams = `?buCode=${body.buCode}`;
     let token = this.cookieService.get('token');
     let userId=this.cookieService.get('userId');
-    console.log(queryParams)
 
-    let stockListURL=this.API_URL+"sd/sl-master/get-max-all"+queryParams;
+    let stockListURL=this.API_URL+"sd/get-stock-price-master"+queryParams;
     const httpOptions = {
       headers: new HttpHeaders({      
        'Content-Type':  'application/json',
        'Authorization': 'Bearer ' + token
       })
     };
-    return this.http.post(stockListURL,'',httpOptions)
+    return this.http.get(stockListURL,httpOptions)
   }
+  getInputValue(){
+    let token = this.cookieService.get('token');
+    let userId=this.cookieService.get('userId');
+    let inputFieldValueURL=this.API_URL+"sd/input";
+    const httpOptions = {
+      headers: new HttpHeaders({      
+       'Content-Type':  'application/json',
+       'Authorization': 'Bearer ' + token
+      })
+    };
+    return this.http.get(inputFieldValueURL,httpOptions)
+  }
+  getUnitCodeDetail(auc0001:any,buc0001:any){
+  const queryParams = `?auc0001=${auc0001}&buc0001=${buc0001}`;
+ let token = this.cookieService.get('token');
+ let userId=this.cookieService.get('userId');
 
+ let stockListURL=this.API_URL+"gm/bu-master/get-bu-info"+queryParams;
+ const httpOptions = {
+   headers: new HttpHeaders({      
+    'Content-Type':  'application/json',
+    'Authorization': 'Bearer ' + token
+   })
+ };
+ return this.http.post(stockListURL,'',httpOptions)
+  }
 }
