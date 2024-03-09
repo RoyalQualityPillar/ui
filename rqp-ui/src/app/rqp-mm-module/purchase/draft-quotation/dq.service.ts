3c50f80e7902eb35bf0faa2202098c0f140d8c8e
Mounika <Admin3@VM27708.winvps.com>
2024-03-09 16:06:51 +0530

    completed pdq,pfq,ppo

--- rqp-ui/src/app/rqp-mm-module/purchase/draft-quotation/dq.service.ts
+++ rqp-ui/src/app/rqp-mm-module/purchase/draft-quotation/dq.service.ts
@@ -1,24 +1,24 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { environment } from '../../../environments/environment';
+import { environment } from 'src/app/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class QuotationService {
+export class DqService {
 
   private API_URL = environment.apiBaseURL;
   constructor(private http: HttpClient, private cookieService: CookieService) { }
   /************************************** DRAFT QUOTATION ************************************************* */
   onSaveUpdate(requestBody: any) {
-    const saveUpdateURL = this.API_URL + 'pmmpqtitem/pmmpqt-items/save-update';
+    const saveUpdateURL = this.API_URL + 'pmmpdq/pmmpdq-items/save-update';
     return this.http.post(saveUpdateURL, requestBody);
   }
 
   getResquestNoID(lc0002: any) { ///1st
     const queryParams = `?lc0002=${lc0002}`;
-    const reviewURL = this.API_URL + 'pmmpqtitem/module-request-no' + queryParams;
+    const reviewURL = this.API_URL + 'pmmpdq/module-request-no' + queryParams;
     return this.http.get(reviewURL);
   }
 }
